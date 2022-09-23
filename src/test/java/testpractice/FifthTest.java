0d5745c0b3c64533e4867d2fdce6bc1f02a9f55a
sncsr <sncsr@LAPTOP-H7R7DK85>
2022-09-23 11:42:05 +0530

    new commit

---
+++ src/test/java/testpractice/FifthTest.java
@@ -0,0 +1,12 @@
+package testpractice;
+
+import org.testng.annotations.Test;
+
+public class FifthTest {
+	@Test
+	public void tc5() {
+		System.out.println(System.getProperty("B"));
+		System.out.println(System.getProperty("U"));
+	}
+
+}
